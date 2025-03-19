9e07c3ef743e15bc8a8d79d2a60afcc817c07680
Gustavo Duara <gugaduara@gmail.com>
2025-03-19 15:04:20 -0300

    implementada logica de lançamento de dado

--- JogodDeDadosConsoleApp/Program.cs
+++ JogodDeDadosConsoleApp/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("______________________________________________________");
+                Console.WriteLine("Jogo dos dados");
+                Console.WriteLine("______________________________________________________");
+
+                Console.Write("Pressione Enter para lançar o dado...");
+                Console.ReadLine();
+
+                Random geradorDeNumeros = new Random();
+
+                int resultado = geradorDeNumeros.Next(1, 7);
+
+                Console.WriteLine($"O número sorteado foi: {resultado}");
+
+                Console.Write("Deseja continuar jogando? (s/n): ");
+                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+
+            }
+
         }
     }
 }
