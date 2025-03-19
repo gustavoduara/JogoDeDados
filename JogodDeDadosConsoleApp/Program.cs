260d7a04ee9c1ece79f4108133bb50beeef75016
Gustavo Duara <gugaduara@gmail.com>
2025-03-19 14:52:21 -0300

    Add project files.

---
+++ JogodDeDadosConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JogodDeDadosConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
